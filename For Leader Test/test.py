59080fd0c1a23531f1ff962e20b9d69ac3d3fdc8
Nikoloz8 <Nikoloztevdoradze08@gmail.com>
2024-12-07 20:54:59 +0400

    Commit

--- For Leader Test/test.py
+++ For Leader Test/test.py
@@ -1,17 +1,35 @@
-def manual_max(list):
-    max_num = list[0]
-    for i in range(len(list)):
-        if list[i] > max_num:
-            max_num = list[i]
-    return max_num
+# def manual_max(list):
+#     max_num = list[0]
+#     for i in range(len(list)):
+#         if list[i] > max_num:
+#             max_num = list[i]
+#     return max_num
 
-print(manual_max([5, 8, 16]))
+# print(manual_max([5, 8, 16]))
 
 
-def manual_min(list):
-    min_num = list[0]
-    for i in range(len(list)):
-        if list[i] < min_num:
-            min_num = list[i]
-    return min_num
-print(manual_min([1, 5, 7]))
+# def manual_min(list):
+#     min_num = list[0]
+#     for i in range(len(list)):
+#         if list[i] < min_num:
+#             min_num = list[i]
+#     return min_num
+# print(manual_min([1, 5, 7]))
+
+
+
+# # 2) შემოატანინეთ მომხმარებელს 10 რიცხვი და გამოიტანეთ მათი ჯამი
+
+
+# num1 = int(input("Enter Your Number1: "))
+# num2 = int(input("Enter Your Number2: "))
+# num3 = int(input("Enter Your Number3: "))
+# num4 = int(input("Enter Your Number4: "))
+# num5 = int(input("Enter Your Number5: "))
+# num6 = int(input("Enter Your Number6: "))
+# num7 = int(input("Enter Your Number7: "))
+# num8 = int(input("Enter Your Number8: "))
+# num9 = int(input("Enter Your Number9: "))
+# num10 = int(input("Enter Your Number10: "))
+
+# print(num1 + num2 + num3 + num4 + num5 + num6 + num7 + num8 + num9 + num10)
