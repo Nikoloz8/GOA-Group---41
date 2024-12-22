2ddcbb7e54da61a923b432c3d06bc86bf7e56755
Nikoloz8 <Nikoloztevdoradze08@gmail.com>
2024-12-23 00:59:56 +0400

    Commit

---
+++ Group 41 CW_HW (My Current Course)/Day 80/Classwork/script.js
@@ -0,0 +1,34 @@
+// Level 80:
+// შექმენით ცვლადები, სადაც შეიტანთ ინფორმაციას თქვენს თავზე, მაგ: სახელი, გვარი, ასაკი და ა.შ
+// შემდეგ ეს ცვლადები დაუმატეთ ერთმანეთს და გამოიტანეთ კონსოლში და ასევე alert - ში.
+
+const name = "Nikoloz"
+const lastname = "Tevdoradze"
+const age = "16"
+
+console.log("My name is " + name + " " + lastname + " and i am " + age + " years old")
+alert("My name is " + name + " " + lastname + " and i am " + age + " years old")
+
+
+// მარტივი კალკულატორი
+// დავალება:
+// დაწერე პროგრამა, რომელიც ითვლის ორი რიცხვის ჯამს, სხვაობას, ნამრავლს და განაყოფს.
+// 'ჯამი': ჯამის ცვლადის დასახელება
+// 'სხვაპბა': სხვაობის ცვლადის დასახელება 
+// გააკეთეთ იგივე სხვა ოპერაციებზეც.
+
+const a = 5
+const b = 16
+const sum = a + b
+const diff = a - b
+const multi = a * b
+const division = a / b
+const quality = a ** b
+
+
+console.log("sum is: " + sum)
+console.log("diff is: " + diff)
+console.log("multiflication is:" + multi)
+console.log("division is: " + division)
+console.log("quality is: " + quality)
+
